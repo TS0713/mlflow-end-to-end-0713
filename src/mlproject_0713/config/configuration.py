667dd0a4989b924997ad9db8d7a440177662fbbe
TS0713 <tsp.0713@gmail.com>
2024-07-26 12:11:01 +0530

    added data validation in configuration manager, schema.yaml - updated

--- src/mlproject_0713/config/configuration.py
+++ src/mlproject_0713/config/configuration.py
@@ -14,6 +14,7 @@ class ConfigurationManager:
                 self.schema = read_yaml(schema_filepath)
                 
                 create_directories([self.config.artifacts_root])
+                
         def get_data_ingestion_config(self) -> DataIngestionConfig:
                 config = self.config.data_ingestion
                 create_directories([config.root_dir])
